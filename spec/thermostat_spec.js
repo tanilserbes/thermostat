4b35ea8c0ea30436dd08c26854c9e4f2db18f016
aasirah93 <62110929+aasirah93@users.noreply.github.com>
2020-05-13 16:30:20 +0100

    increase and decrease temperature functionality

--- spec/thermostat_spec.js
+++ spec/thermostat_spec.js
@@ -9,5 +9,12 @@ describe('Thermostat', function() {
   it('starts at 20 degrees', function() {
     expect(thermostat.getCurrentTemperature()).toEqual(20);
   }); 
+  it('increases temperature with up', function() {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
 });
-
+it('decreases temperature with down', function() {
+  thermostat.down();
+  expect(thermostat.getCurrentTemperature()).toEqual(19);
+});
+});
--- src/thermostat.js
+++ src/thermostat.js
@@ -3,8 +3,16 @@ class Thermostat {
 
 constructor() {
   this.temperature = 20;
+  
 }
+
   getCurrentTemperature(){
     return this.temperature;
+  }
+  up(){
+  return this.temperature++;
+  }
+  down(){
+    return this.temperature--;
   }
 }
