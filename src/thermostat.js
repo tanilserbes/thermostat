a2643893f14933d48b8f94ff20349ef6d54e4e4a
tanilserbes <serbestanil@gmail.com>
2020-05-14 00:57:45 +0100

    thermostat energy usage

--- src/thermostat.js
+++ src/thermostat.js
@@ -8,6 +8,8 @@ constructor() {
   this.MAX_LIMIT_PSM_ON = 25;
   this.MAX_LIMIT_PSM_OFF = 32;
   this.DEFAULT_TEMPERATURE = 20;
+  this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
+
 }
 
 isMinimumTemperature() {
@@ -59,4 +61,13 @@ isMinimumTemperature() {
     return this.temperature = this.DEFAULT_TEMPERATURE
   }
 
+  energyUsage() {
+    if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+      return 'low-usage';
+    }
+    if (this.temperature >= this.MEDIUM_ENERGY_USAGE_LIMIT && this.temperature <= this.MAX_LIMIT_PSM_ON) {
+      return 'medium-usage';
+    }
+      return 'high-usage';
+  }
 }
