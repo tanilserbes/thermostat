a5d5af38165f3f5ba24d395a3185542baf39367e
GarethWoodman <gareth.woodman92@gmail.com>
2020-05-14 15:18:31 +0100

    Added Jquery interface for thermostat

---
+++ src/interface.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  var thermostat = new Thermostat();
+  updateTemperature();
+
+  $('#temperature-up').on('click', function() {
+    thermostat.up();
+    updateTemperature();
+  })
+
+  $('#temperature-down').on('click', function() {
+    thermostat.down();
+    updateTemperature();
+  })
+
+  $('#temperature-reset').click(function() {
+    thermostat.resetTemperature();
+    updateTemperature();
+  });
+
+  $('#powersaving-on').click(function() {
+    thermostat.switchPowerSavingModeOn();
+    $('#power-saving').text('on')
+    updateTemperature();
+  })
+
+  $('#powersaving-off').click(function() {
+    thermostat.switchPowerSavingModeOff();
+    $('#power-saving').text('off')
+    updateTemperature();
+  })
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.temperature);
+    $('#temperature').attr('class', thermostat.energyUsage());
+  };
+});
