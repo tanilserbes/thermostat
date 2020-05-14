2d8dfdd23b9bf0a77ff07b91b60b87a986ead0aa
tanilserbes <serbestanil@gmail.com>
2020-05-14 16:37:27 +0100

    added Thermostat: APIs

--- src/interface.js
+++ src/interface.js
@@ -19,16 +19,30 @@ $(document).ready(function() {
 
   $('#powersaving-on').click(function() {
     thermostat.switchPowerSavingModeOn();
-    $('#power-saving').text('on')
+    $('#power-saving-status').text('on')
     updateTemperature();
   })
 
   $('#powersaving-off').click(function() {
     thermostat.switchPowerSavingModeOff();
-    $('#power-saving').text('off')
+    $('#power-saving-status').text('off')
     updateTemperature();
   })
 
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    displayWeather(city) 
+  })
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+    })
+  }
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.energyUsage());
